77b34c005b951f51e3573d3f30e55b7ef7b77511
codiaji <codiaji@gmail.com>
2022-11-24 15:13:07 +0100

    Type Assertions

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
-interface Person {
- firstName: string;
-};
-interface Person {
- lastName: string;
-};
-const me:Person = {firstName:"Amine",lastName:"El idrissi"};
-console.log('me :>> ', me);
-// The below code snippet will lead to error
-/* type Window = {
-    title: string
-   }
-   type Window = {
-    ts: TypeScriptAPI
-   } */
+// Type Assertions
+let myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement
+myCanvas = <HTMLCanvasElement>document.getElementById("main_canvas")
+// type assertions convert to a more specific or less specific version of a type. This rule prevents "impossible" coercions like:
+// const x = "hello" as number;
+// A workaround of the above rule is as below
+// const a = (expr as any) as T;
