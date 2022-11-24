e19d6121f4d75bee0d45647e750778e7e30e2f2e
codiaji <codiaji@gmail.com>
2022-11-24 14:34:14 +0100

    object types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-let obj: any = { x: 0 };
-// None of the following lines of code will throw compiler errors.
-// Using `any` disables all further type checking, and it is assumed
-// you know the environment better than TypeScript.
-obj.foo();
-obj();
-obj.bar = 100;
-obj = "hello";
-const n: number = obj;
+// The parameter's type annotation is an object type
+function printCoord(pt: { x: number; y: number, z?:number }) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+   }
+   printCoord({ x: 3, y: 7 });
+   printCoord({ x: 3, y: 7,z:11 });
