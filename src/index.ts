9d1a40e506bb187e2f7fd108f3aeb0426a902bcf
codiaji <codiaji@gmail.com>
2022-11-26 11:10:52 +0100

    Non-null Assertion Operator (Postfix ! )

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-// strictNullChecks on
-// we can use narrowing to check for values that might be null :
-function doSomething(x: string | null) {
-    if (x === null) {
-    // do nothing
-    } else {
-    console.log("Hello, " + x.toUpperCase());
-    }
-}
-doSomething(null);
-// The below line will lead to an error once uncommented
-//    doSomething(undefined);
+function liveDangerously(x?: number | null) {
+    // No error
+    console.log(x!.toFixed());
+   }
