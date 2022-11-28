6cdcdd3f0e1e40bcd80de35b20b811b2b35765dc
codiaji <codiaji@gmail.com>
2022-11-28 18:07:09 +0100

    Control flow analysis

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-let x = Math.random() < 0.5 ? 10 : 'hello world!';
-x = 1;
-console.log(`x's type's: ${typeof x}`);
-x = 'goodbye!';
-console.log(`x's type's: ${typeof x}`);
+function example() {
+	let x: string | number | boolean;
+	x = Math.random() < 0.5;
+	console.log(x);
+	if (Math.random() < 0.5) {
+		x = 'hello';
+		console.log(x);
+	} else {
+		x = 100;
+		console.log(x);
+	}
+	return x;
+}
