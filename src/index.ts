05cf37bc3c0e2352f469bd53070f9432712db746
codiaji <codiaji@gmail.com>
2022-11-28 15:42:13 +0100

    The in operator narrowing

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-interface Container {
- value: number | null | undefined;
+type vFT = () => void;
+type voidFunctionType = () => {};
+type Fish = { swim: vFT };
+type Bird = { fly: vFT };
+type Human = { swim?: vFT; fly?: vFT };
+function move(animal: Fish | Bird | Human) {
+	if ('swim' in animal) {
+		return animal;
+	} else {
+		return animal;
+	}
 }
-function multiplyValue(container: Container, factor: number) {
- // Remove both 'null' and 'undefined' from the type.
- if (container.value != null) {
- console.log(container.value);
- // Now we can safely multiply 'container.value'.
- container.value *= factor;
- }
-}
-let container:Container={value:974/2}
-multiplyValue(container,2)
-console.log('container.value :>> ', container.value);
-container={value:undefined}
-multiplyValue(container,2)
-container={value:null}
-multiplyValue(container,2)
+console.log('move({ fly: () => {} }) :>> ', move({ fly: () => {} }));
+console.log('move({ swim: () => {} }) :>> ', move({ swim: () => {} }));
