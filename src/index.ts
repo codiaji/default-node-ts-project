1366c90b6c402bebd44c8a960d19f827eab5deb3
codiaji <codiaji@gmail.com>
2022-12-03 11:40:24 +0100

    Using type predicates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-function example() {
-	let x: string | number | boolean;
-	x = Math.random() < 0.5;
-	console.log(x);
-	if (Math.random() < 0.5) {
-		x = 'hello';
-		console.log(x);
-	} else {
-		x = 100;
-		console.log(x);
-	}
-	return x;
+type vFT = () => void;
+type Fish = { name: string; swim: vFT };
+type Bird = { name: string; fly: vFT };
+function isFish(pet: Fish | Bird): pet is Fish {
+	return (pet as Fish).swim !== undefined;
 }
+function getSmallPet() {
+	if (Math.random() > 0.5) {
+		return { name: 'bird', fly: () => {} } as Bird;
+	} else {
+		return { name: 'fish', swim: () => {} } as Fish;
+	}
+}
+
+// Both calls to 'swim' and 'fly' are now okay.
+let pet = getSmallPet();
+if (isFish(pet)) {
+	pet.swim();
+} else {
+	pet.fly();
+}
+
+const zoo: (Fish | Bird)[] = [getSmallPet(), getSmallPet(), getSmallPet()];
+const underWater3: Fish[] = zoo.filter((pet): pet is Fish => {
+	if (pet.name === 'sharkey') return false;
+	return isFish(pet);
+});
