5d2c2a99b5bd01c0b1af91c52a6e250d3bf6ecf4
codiaji <codiaji@gmail.com>
2022-12-05 12:03:37 +0100

    Function Type Expressions

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
-interface Circle {
-	kind: 'circle';
-	radius: number;
+type GreetFunction = (a: string) => void;
+function greeter(fn: GreetFunction) {
+	fn('Hello, World');
 }
-interface Square {
-	kind: 'square';
-	sideLength: number;
+function printToConsole(s: string) {
+	console.log(s);
 }
-type Shape = Circle | Square;
-
-function getArea(shape: Shape) {
-	switch (shape.kind) {
-		case 'circle':
-			return Math.PI * shape.radius ** 2;
-		case 'square':
-			return shape.sideLength ** 2;
-		default:
-			const _exhaustiveCheck: never = shape;
-			return _exhaustiveCheck;
-	}
-}
+greeter(printToConsole);
