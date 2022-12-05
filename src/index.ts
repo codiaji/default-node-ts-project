5caa8c32c4aa88bd84396f06f4722889c082a05a
codiaji <codiaji@gmail.com>
2022-12-05 12:40:16 +0100

    Generic Functions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-interface CallOrConstruct {
-	new (s: string): Date;
-	(n?: number): number;
+function firstElement<Type>(arr: Type[]): Type | undefined {
+	return arr[0];
 }
+console.log('firstElement([]) :>> ', firstElement([]));
+console.log('firstElement([1]) :>> ', firstElement([1]));
+console.log('firstElement(["a","b"]) :>> ', firstElement(['a', 'b']));
