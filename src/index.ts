abf4b9d319b075d6908ccd245ca937840b898849
codiaji <codiaji@gmail.com>
2022-12-05 11:21:32 +0100

    Discriminated unions

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,18 @@
-type vFT = () => void;
-type Fish = { name: string; swim: vFT };
-type Bird = { name: string; fly: vFT };
-function isFish(pet: Fish | Bird): pet is Fish {
-	return (pet as Fish).swim !== undefined;
+interface Circle {
+	kind: 'circle';
+	radius: number;
 }
-function getSmallPet() {
-	if (Math.random() > 0.5) {
-		return { name: 'bird', fly: () => {} } as Bird;
-	} else {
-		return { name: 'fish', swim: () => {} } as Fish;
+interface Square {
+	kind: 'square';
+	sideLength: number;
+}
+type Shape = Circle | Square;
+
+function getArea(shape: Shape) {
+	switch (shape.kind) {
+		case 'circle':
+			return Math.PI * shape.radius ** 2;
+		case 'square':
+			return shape.sideLength ** 2;
 	}
 }
-
-// Both calls to 'swim' and 'fly' are now okay.
-let pet = getSmallPet();
-if (isFish(pet)) {
-	pet.swim();
-} else {
-	pet.fly();
-}
-
-const zoo: (Fish | Bird)[] = [getSmallPet(), getSmallPet(), getSmallPet()];
-const underWater3: Fish[] = zoo.filter((pet): pet is Fish => {
-	if (pet.name === 'sharkey') return false;
-	return isFish(pet);
-});
