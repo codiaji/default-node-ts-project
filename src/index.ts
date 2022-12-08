6176582a2061d14f7cb7a79d0e2ba0a61a578659
codiaji <codiaji@gmail.com>
2022-12-08 11:21:37 +0100

    Generic Functions- Specifying Type Arguments

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-function minimumLength<Type extends { length: number }>(
-	obj: Type,
-	minimum: number,
-): Type {
-	if (obj.length >= minimum) {
-		return obj;
-	} else {
-		return { length: minimum };
-	}
+function combine<Type>(arr1: Type[], arr2: Type[]): Type[] {
+	return arr1.concat(arr2);
 }
-// 'arr' gets value { length: 6 }
-const arr = minimumLength([1, 2, 3], 6);
-// and crashes here because arrays have
-// a 'slice' method, but not the returned object!
-console.log(arr.slice(0));
+const arr = combine<string | number>([1, 2, 3], ['hello']);
