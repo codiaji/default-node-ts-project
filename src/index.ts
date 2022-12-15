6da45cdcf54cc70ef96d772cd1696abf9704f964
codiaji <codiaji@gmail.com>
2022-12-15 14:57:57 +0100

    Function Overloads

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
-	for (let i = 0; i < arr.length; i++) {
-		callback(arr[i], i);
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+	if (d !== undefined && y !== undefined) {
+		return new Date(y, mOrTimestamp, d);
+	} else {
+		return new Date(mOrTimestamp);
 	}
 }
-
-// If we uncomment the below code snippet, we'll get the following error: 'i' is possibly 'undefined'.
-/* 
-myForEach([1, 2, 3], (a, i) => {
-	console.log(i.toFixed());
-   }); 
-*/
+const d1 = makeDate(12345678);
+const d2 = makeDate(5, 5, 5);
+// If we uncomment the below code snippet, we'll get the following error:
+// const d3 = makeDate(1, 3);
