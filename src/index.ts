4db441cb6771468f1d2dc25620927843c2933ec5
codiaji <codiaji@gmail.com>
2022-12-15 13:20:13 +0100

    Generic Functions- Use Fewer Type Parameters

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-function firstElement1<Type>(arr: Type[]) {
-	return arr[0];
+function filter1<Type>(arr: Type[], func: (arg: Type) => boolean): Type[] {
+	return arr.filter(func);
 }
-function firstElement2<Type extends any[]>(arr: Type) {
-	return arr[0];
+function filter2<Type, Func extends (arg: Type) => boolean>(
+	arr: Type[],
+	func: Func,
+): Type[] {
+	return arr.filter(func);
 }
-// a: number (good)
-const a = firstElement1([1, 2, 3]);
-// b: any (bad)
-const b = firstElement2([1, 2, 3]);
