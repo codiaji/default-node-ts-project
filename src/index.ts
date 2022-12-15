fb408705a936b9db5c0851b365c6d444d4bf7dcc
codiaji <codiaji@gmail.com>
2022-12-15 14:42:16 +0100

    Optional Parameters in Callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-function f(n = 10) {
-	console.log(n.toFixed()); // 0 arguments
-	console.log(n.toFixed(3)); // 1 argument
+function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
+	for (let i = 0; i < arr.length; i++) {
+		callback(arr[i], i);
+	}
 }
+
+// If we uncomment the below code snippet, we'll get the following error: 'i' is possibly 'undefined'.
+/* 
+myForEach([1, 2, 3], (a, i) => {
+	console.log(i.toFixed());
+   }); 
+*/
