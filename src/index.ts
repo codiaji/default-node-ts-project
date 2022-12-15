e8680bc5dea3e05eb0062ce5eb36a7218fc7c5ee
codiaji <codiaji@gmail.com>
2022-12-15 13:25:52 +0100

    Generic Functions-  Type Parameters Should Appear Twice

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-function filter1<Type>(arr: Type[], func: (arg: Type) => boolean): Type[] {
-	return arr.filter(func);
+// The below commented code snippet show a case when to not use type parameters (i.e. when type parameters aren't relating the types of multiple value), and uncommented one, show what we should do instead
+/* 
+function greet<Str extends string>(s: Str) {
+ console.log("Hello, " + s);
 }
-function filter2<Type, Func extends (arg: Type) => boolean>(
-	arr: Type[],
-	func: Func,
-): Type[] {
-	return arr.filter(func);
+*/
+function greet(s: string) {
+	console.log('Hello, ' + s);
 }
