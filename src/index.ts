5014de180dcc2ebfeb189033b2a9f853a403206d
codiaji <codiaji@gmail.com>
2023-01-02 17:05:12 +0100

    Parameter Destructuring

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-arr1.push(...arr2);
-
-// Code this
-// Inferred as 2-length tuple
-const args = [8, 5] as const;
-// OK
-const angle = Math.atan2(...args);
-
-// But not that
-// Inferred type is number[] -- "an array with zero or more numbers",
-// not specifically two numbers
-/* const args = [8, 5];
-const angle = Math.atan2(...args); */
+type ABC = { a: number; b: number; c: number };
+function sum({ a, b, c }: ABC) {
+	console.log(a + b + c);
+}
