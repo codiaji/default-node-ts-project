00c88b90303fb303bc3cd40a92a614b7b48dddb6
codiaji <codiaji@gmail.com>
2023-01-02 12:15:46 +0100

    Rest Arguments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-function multiply(n: number, ...m: number[]) {
-	return m.map(x => n * x);
-}
-// 'a' gets value [10, 20, 30, 40]
-const a = multiply(10, 1, 2, 3, 4);
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+arr1.push(...arr2);
+
+// Code this
+// Inferred as 2-length tuple
+const args = [8, 5] as const;
+// OK
+const angle = Math.atan2(...args);
+
+// But not that
+// Inferred type is number[] -- "an array with zero or more numbers",
+// not specifically two numbers
+/* const args = [8, 5];
+const angle = Math.atan2(...args); */
