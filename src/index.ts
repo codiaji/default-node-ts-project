fbe9e5d14e5f845b280df79bd96d84dc2dcc33e7
codiaji <codiaji@gmail.com>
2023-01-02 11:57:34 +0100

    unknown

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-function getRndmObj(): object {
-	const rndm = Math.random();
-	return rndm ? [rndm] : { rndm };
+function f1(a: any) {
+	a.b(); // OK
 }
+function f2(a: unknown) {
+	console.log(a);
+	// uncommenting the following line will lead to a typescript error
+	// a.b();
+}
+
+function safeParse(s: string): unknown {
+	return JSON.parse(s);
+}
+// Need to be careful with 'obj'!
+const obj = safeParse((() => true).toString());
