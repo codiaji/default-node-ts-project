e9071e3a70787f007508623dc4ec05fe03633ca9
codiaji <codiaji@gmail.com>
2023-01-02 11:42:30 +0100

    Declaring this in a Function

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-// Don't write code like the below one
-function len(s: string): number;
-function len(arr: any[]): number;
-function len(x: any) {
-	return x.length;
-}
+const user = {
+	id: 123,
+	admin: false,
+	becomeAdmin: function () {
+		this.admin = true;
+	},
+};
 
-len(''); // OK
-len([0]); // OK
-len(Math.random() > 0.5 ? 'hello' : [0]);
-// Write this alternative
-/* function len(x: any[] | string) {
-	return x.length;
-   } */
+//...
+interface DB {
+	filterUsers(filter: (this: User) => boolean): User[];
+}
+const db = getDB();
+const admins = db.filterUsers(function (this: User) {
+	return this.admin;
+});
+// don't use arrow function, It won't work
+// const admins = db.filterUsers(() => this.admin);
