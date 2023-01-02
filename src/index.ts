cbb793e3e9f8bcd7bfe93829450cfb8320c2fea2
codiaji <codiaji@gmail.com>
2023-01-02 12:02:19 +0100

    never

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-function f1(a: any) {
-	a.b(); // OK
+function fail(msg: string): never {
+	throw new Error(msg);
 }
-function f2(a: unknown) {
-	console.log(a);
-	// uncommenting the following line will lead to a typescript error
-	// a.b();
+function fn(x: string | number) {
+	if (typeof x === 'string') {
+		// do something
+	} else if (typeof x === 'number') {
+		// do something else
+	} else {
+		x; // has type 'never'!
+	}
 }
-
-function safeParse(s: string): unknown {
-	return JSON.parse(s);
-}
-// Need to be careful with 'obj'!
-const obj = safeParse((() => true).toString());
