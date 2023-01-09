eef04813acc52216df870c9703044733b82af47b
codiaji <codiaji@gmail.com>
2023-01-09 10:51:51 +0100

    Property Modifiers-  readonly Properties

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-interface Shape {
-	//...
+interface Home {
+	readonly resident: { name: string; age: number };
 }
-interface PaintOptions {
-	shape: Shape;
-	xPos?: number;
-	yPos?: number;
+function visitForBirthday(home: Home) {
+	// We can read and update properties from 'home.resident'.
+	console.log(`Happy birthday ${home.resident.name}!`);
+	home.resident.age++;
 }
+// function evict(home: Home) {
+// But we can't write to the 'resident' property itself on a 'Home'.
+// 	home.resident = {
+// 		name: 'Victor the Evictor',
+// 		age: 42,
+// 	};
+// }
