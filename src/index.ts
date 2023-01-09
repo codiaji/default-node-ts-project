f12721093c859d13a39b1819fb901820697b9d0d
codiaji <codiaji@gmail.com>
2023-01-09 10:55:52 +0100

    Property Modifiers-  readonly Properties "changeable via aliasin"

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-interface Home {
-	readonly resident: { name: string; age: number };
+// readonly properties can also change via aliasing.
+interface Person {
+	name: string;
+	age: number;
 }
-function visitForBirthday(home: Home) {
-	// We can read and update properties from 'home.resident'.
-	console.log(`Happy birthday ${home.resident.name}!`);
-	home.resident.age++;
+interface ReadonlyPerson {
+	readonly name: string;
+	readonly age: number;
 }
-// function evict(home: Home) {
-// But we can't write to the 'resident' property itself on a 'Home'.
-// 	home.resident = {
-// 		name: 'Victor the Evictor',
-// 		age: 42,
-// 	};
-// }
+let writablePerson: Person = {
+	name: 'Person McPersonface',
+	age: 42,
+};
+// works
+let readonlyPerson: ReadonlyPerson = writablePerson;
+console.log(readonlyPerson.age); // prints '42'
+writablePerson.age++;
+console.log(readonlyPerson.age); // prints '43'
