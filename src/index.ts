ca23d67a69916a4bdf9a7fb13df2d09b892d0ab3
codiaji <codiaji@gmail.com>
2023-01-26 16:01:30 +0100

    Extending Types

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,11 @@
-// The below code snippet will lead to error
-/* 
-interface ReadonlyStringArray {
-	readonly [index: number]: string;
+interface Colorful {
+	color: string;
 }
-let myArray: ReadonlyStringArray = ['AE10', 'EA7'];
-myArray[1] = 'ea7'; 
-*/
-interface ReadonlyStringArray {
-	readonly [index: number]: string;
+interface Circle {
+	radius: number;
 }
-let myArray: ReadonlyStringArray = ['AE10', 'EA7'];
-myArray = ['ea7', 'ae10'];
-// The below code snippet will lead to error
-// myArray.push('am');
-// The below code snippet will lead to error
-// console.log('[...myArray,"am"] :>> ', [...myArray, 'am']);
-// The below code snippet will lead to error
-// Array.from(myArray);
+interface ColorfulCircle extends Colorful, Circle {}
+const cc: ColorfulCircle = {
+	color: 'red',
+	radius: 42,
+};
