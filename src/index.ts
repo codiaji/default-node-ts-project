e37ee2331acf38228f654527c28df4f7809e250e
codiaji <codiaji@gmail.com>
2023-01-27 11:59:58 +0100

    The ReadonlyArray Type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-function doSomething(value: Array<string>) {
-	// ...
+// The bellow line is equivalent to the following one
+// function doStuff(values: readonly string[]) {
+function doStuff(values: ReadonlyArray<string>) {
+	// We can read from 'values'...
+	const copy = values.slice();
+	console.log(`The first value is ${values[0]}`);
+	// ...but we can't mutate 'values' as in the commented below code snippet
+	//  values.push("hello!");
 }
-let myArray: string[] = ['hello', 'world'];
-// either of these work!
-doSomething(myArray);
-doSomething(new Array('hello', 'world'));
+const roArray: ReadonlyArray<string> = ['red', 'green', 'blue'];
+// assignability isn't bidirectional between regular Array s and ReadonlyArray
+let x: readonly string[] = [];
+let y: string[] = [];
+x = y;
+y = x;
