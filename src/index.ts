a48f3eddc5d891fc95fbf7f32538574d3241fba6
codiaji <codiaji@gmail.com>
2023-01-27 11:31:19 +0100

    Generic Object Types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-interface Colorful {
-	color: string;
+interface Box<Type> {
+	contents: Type;
 }
-interface Circle {
-	radius: number;
+interface StringBox {
+	contents: string;
 }
-type ColorfulCircle = Colorful & Circle;
+let boxA: Box<string> = { contents: 'hello' };
+boxA.contents;
+
+interface Apple {
+	// ....
+}
+// Same as '{ contents: Apple }'.
+type AppleBox = Box<Apple>;
+function setContents<Type>(box: Box<Type>, newContents: Type) {
+	box.contents = newContents;
+}
+type OrNull<Type> = Type | null;
+type OneOrMany<Type> = Type | Type[];
+type OneOrManyOrNull<Type> = OrNull<OneOrMany<Type>>;
+type OneOrManyOrNullStrings = OneOrManyOrNull<string>;
