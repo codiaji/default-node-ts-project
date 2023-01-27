264ea8b4ab7ba1493788b2ddccba30c14b7f231b
codiaji <codiaji@gmail.com>
2023-01-27 11:39:46 +0100

    The "Array" Type

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-interface Box<Type> {
-	contents: Type;
+function doSomething(value: Array<string>) {
+	// ...
 }
-interface StringBox {
-	contents: string;
-}
-let boxA: Box<string> = { contents: 'hello' };
-boxA.contents;
-
-interface Apple {
-	// ....
-}
-// Same as '{ contents: Apple }'.
-type AppleBox = Box<Apple>;
-function setContents<Type>(box: Box<Type>, newContents: Type) {
-	box.contents = newContents;
-}
-type OrNull<Type> = Type | null;
-type OneOrMany<Type> = Type | Type[];
-type OneOrManyOrNull<Type> = OrNull<OneOrMany<Type>>;
-type OneOrManyOrNullStrings = OneOrManyOrNull<string>;
+let myArray: string[] = ['hello', 'world'];
+// either of these work!
+doSomething(myArray);
+doSomething(new Array('hello', 'world'));
