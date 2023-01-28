bcb2197756b3aa322342c436fd164dc7d65fe9eb
codiaji <codiaji@gmail.com>
2023-01-28 12:15:34 +0100

    Tuple Types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-// The bellow line is equivalent to the following one
-// function doStuff(values: readonly string[]) {
-function doStuff(values: ReadonlyArray<string>) {
-	// We can read from 'values'...
-	const copy = values.slice();
-	console.log(`The first value is ${values[0]}`);
-	// ...but we can't mutate 'values' as in the commented below code snippet
-	//  values.push("hello!");
+//
+type StringNumberPair = [string, number];
+// function doSomething(stringHash: StringNumberPair) {
+function doSomething(stringHash: [string, number]) {
+	const [inputString, hash] = stringHash;
+	console.log(inputString);
+	console.log(hash);
+	//Tuples don't allow index past the number of elements
+	// stringHash[2] = true;
 }
-const roArray: ReadonlyArray<string> = ['red', 'green', 'blue'];
-// assignability isn't bidirectional between regular Array s and ReadonlyArray
-let x: readonly string[] = [];
-let y: string[] = [];
-x = y;
-y = x;
+type Either2dOr3d = [number, number, number?];
+// Optional tuple elements can only come at the end, and also affect the type of length.
+function getCoordinate(coord: Either2dOr3d) {
+	const [x, y, z] = coord;
+	console.log(`Provided coordinates had ${coord.length} dimensions`);
+	console.log(`the coordinates are x=${x},y=${y} and z=${z}`);
+}
+// Tuples can also have rest elements, which have to be an array/tuple type
+type activityClub = [...StringNumberPair, ...string[]];
+type StringNumberBooleans = [string, number, ...boolean[]];
+type StringBooleansNumber = [string, ...boolean[], number];
+type BooleansStringNumber = [...boolean[], string, number];
