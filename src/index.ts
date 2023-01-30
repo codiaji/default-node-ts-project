419ad023ab642c6775fcfa5a74070f0edb24d092
codiaji <codiaji@gmail.com>
2023-01-30 11:20:50 +0100

    Generics- generic interface

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
-type ReadOnlyStringNumberPair = readonly [string, number];
-// function doSomething(pair: readonly [string, number]) {
-function doSomething(pair: ReadOnlyStringNumberPair) {
-	// Uncommenting the bellow code snippet will lead to error
-	// pair[0] = 'hello!';
-	const [name, age] = pair;
-	console.log('name :>> ', name);
-	console.log('age :>> ', age);
+interface GenericIdentityFn {
+	<Type>(arg: Type): Type;
 }
-// array literals with const assertions will be inferred with readonly tuple types
-let point = [3, 4] as const;
-function distanceFromOrigin([x, y]: [number, number]) {
-	return Math.sqrt(x ** 2 + y ** 2);
+function identity<Type>(arg: Type): Type {
+	return arg;
 }
-// Uncommenting the bellow code will introduce an error
-// distanceFromOrigin(point);
+let myIdentity: GenericIdentityFn = identity;
