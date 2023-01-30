18202ee8df294c65a08c432d799bfd4c7d18a88f
codiaji <codiaji@gmail.com>
2023-01-30 16:17:25 +0100

    Generics- Generic Classes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-interface GenericIdentityFn<Type> {
-	(arg: Type): Type;
+/* 
+Generic classes are only generic over their instance side rather than their static side
+*/
+class GenericNumber<NumType> {
+	zeroValue: NumType;
+	add: (x: NumType, y: NumType) => NumType;
+	log: () => void;
 }
-function identity<Type>(arg: Type): Type {
-	return arg;
-}
-let myIdentity: GenericIdentityFn<number> = identity;
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) {
+	return x + y;
+};
+myGenericNumber.log = function () {
+	console.log(this);
+};
+myGenericNumber.log();
